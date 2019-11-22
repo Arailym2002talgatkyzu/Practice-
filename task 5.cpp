2594e7f0c48a528d355266655f24e6aefb160999
Arailym <53095803+Arailym2002talgatkyzu@users.noreply.github.com>
2019-11-22 23:40:47 +0600

    Add files via upload

---
+++ task 5.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+int main() {
+	
+	for (int a=3, i = 2; a<999,i <= sqrt(a+i); a++, ++i) {
+		if (a % i != 0) {
+			cout <<a<<" "<<a+2 << endl;
+			
+		}
+	return 0;}
+	return 0;
+}
